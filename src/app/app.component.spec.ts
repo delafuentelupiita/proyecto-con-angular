44f1d1b857a36d1a6595bf234ec3e21ac81b631b
delafuentelupiita <delafuentelupiita@gmail.com>
2025-05-13 00:38:42 -0600

    Configuracion del proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'temp-project' title`, () => {
+  it(`should have the 'proyecto-con-angular' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('temp-project');
+    expect(app.title).toEqual('proyecto-con-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, temp-project');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, proyecto-con-angular');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'temp-project';
+  title = 'proyecto-con-angular';
+  nombre = 'Lupita Mtz.'
 }
